42d6030befdb64487f2607180622d49b07be0c9e
Johannes Fahrenkrug <jfahrenkrug@gmail.com>
2014-03-03 14:57:02 -0500

    Create SPWKAppDelegate.m

---
+++ KVOMemoryLeak/SPWKAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  SPWKAppDelegate.m
+//  KVOMemoryLeak
+//
+//  Created by Fahrenkrug, Johannes on 3/3/14.
+//  Copyright (c) 2014 Springenwerk. All rights reserved.
+//
+
+#import "SPWKAppDelegate.h"
+
+@implementation SPWKAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    // Override point for customization after application launch.
+    return YES;
+}
+
+@end
