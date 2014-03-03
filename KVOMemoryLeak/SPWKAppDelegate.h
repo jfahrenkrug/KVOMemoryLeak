f3823e368d4b5344d6d35f668e05ce67cc57bc56
Johannes Fahrenkrug <jfahrenkrug@gmail.com>
2014-03-03 14:56:29 -0500

    Create SPWKAppDelegate.h

---
+++ KVOMemoryLeak/SPWKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SPWKAppDelegate.h
+//  KVOMemoryLeak
+//
+//  Created by Fahrenkrug, Johannes on 3/3/14.
+//  Copyright (c) 2014 Springenwerk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPWKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
