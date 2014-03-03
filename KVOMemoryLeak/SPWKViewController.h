335616ab60f89151e0cbe3b4e45bea7570b33ba6
Johannes Fahrenkrug <jfahrenkrug@gmail.com>
2014-03-03 14:57:21 -0500

    Create SPWKViewController.h

---
+++ KVOMemoryLeak/SPWKViewController.h
@@ -0,0 +1,13 @@
+//
+//  SPWKViewController.h
+//  KVOMemoryLeak
+//
+//  Created by Fahrenkrug, Johannes on 3/3/14.
+//  Copyright (c) 2014 Springenwerk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPWKViewController : UIViewController
+
+@end
